40108d6fd055edd72ea4247baa60863ab986ef14
Alexei Bushuev <alexeib.2018@gmail.com>
2018-11-03 18:48:07 +0300

    removed day_of_week from excel_orders table

--- sql/excel_import.sql
+++ sql/excel_import.sql
@@ -2,14 +2,12 @@ CREATE TABLE public.excel_orders (
     id integer NOT NULL,
     account text NOT NULL,
     item_no text NOT NULL,
-    day_of_week text NOT NULL,
     location text NOT NULL,
     quantity integer NOT NULL,
     active boolean DEFAULT true,
     shipment_date date,
     date_created timestamp DEFAULT now(),
     item_active boolean DEFAULT true,
-    CONSTRAINT excel_orders_day_of_week_check CHECK ((day_of_week = ANY (ARRAY['sunday'::text, 'monday'::text, 'tuesday'::text, 'wednesday'::text, 'thursday'::text, 'friday'::text, 'saturday'::text]))),
     CONSTRAINT excel_orders_quantity_check CHECK ((quantity > 0))
 );
 
